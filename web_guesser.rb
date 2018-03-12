092292699783e6eb586b456bc022400dcd69c279
salemvelic <velicsalem@gmail.com>
2018-03-12 18:41:04 +0000

    I4: colorize output

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,19 +8,30 @@ def check_guess(guess = "")
 
     if guess - 5 > SECRET_NUMBER
         message = "Way too high"
+        color = "red"
+        return message, color
     elsif guess > SECRET_NUMBER
-        message = "Too high"    
+        message = "Too high"
+        color = "DarkRed"
+        return message, color    
     elsif guess + 5 < SECRET_NUMBER
         message = "Way too low"
+        color = "FireBrick"
+        return message, color
     elsif guess < SECRET_NUMBER
-        message = " Too low"    
+        message = " Too low" 
+        color = "IndianRed"
+        return message, color   
     else 
         message = "You got it right!"
+        color = "green"
+        return message, color
     end    
 end
 
 get '/' do
+  color = "red"
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => { :number => SECRET_NUMBER, :message => message}
+  message, color = check_guess(guess)
+  erb :index, :locals => { :number => SECRET_NUMBER, :message => message, :color => color}
 end
